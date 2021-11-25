7c3cd082501f04b6a1ac68ebfa449c0ca2c181a5
ishaanp9 <41341006+ishaanp9@users.noreply.github.com>
2021-11-24 18:23:44 -0800

    Initialize Project and added basic components
    Added skeleton html w/ minimal styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <p className="header-item">Home</p>
+        <p className="header-item">About</p>
+        <p className="header-item">Resume</p>
+        <p className="header-item">Projects</p>
+        <p className="header-item">Contact Me</p>
+      </div>
+
+      {/* Here we gonna have my name */}
+
+      <h1>Ishaan Puri</h1>
+      <div>
+        <AiFillGithub classname="link-logo" name="github-logo" />
+        <AiFillLinkedin classname="link-logo" name="linkedin-logo" />
+        <MdEmail classname="link-logo" name="email-logo" />
+        <AiFillInstagram classname="link-logo" name="instagram-logo" />
+      </div>
+
+      {/* Here will be the buttons to my github, linkedin, email, and instagram */}
     </div>
   );
 }
