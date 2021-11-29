e4c6169b2a1a8b77af72133eae5411381a4cb76d
ishaanp9 <41341006+ishaanp9@users.noreply.github.com>
2021-11-28 20:41:35 -0800

    more updates -> div styling and some additional components
    Added some css and some component styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai';
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
 import { MdEmail } from 'react-icons/md';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 function App() {
   return (
@@ -15,12 +16,69 @@ function App() {
 
       {/* Here we gonna have my name */}
 
-      <h1>Ishaan Puri</h1>
-      <div>
-        <AiFillGithub classname="link-logo" name="github-logo" />
-        <AiFillLinkedin classname="link-logo" name="linkedin-logo" />
-        <MdEmail classname="link-logo" name="email-logo" />
-        <AiFillInstagram classname="link-logo" name="instagram-logo" />
+      <div className="name-text">Ishaan Puri</div>
+      <h3 className="subtitle-text">
+        <span className="subtitle-item-text">Software Developer</span>
+        <span className="subtitle-item-text">Entrepenuer</span>
+        <span className="subtitle-item-text">Learner</span>
+      </h3>
+      <div className="links-container">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/ishaanp9"
+        >
+          <div className="link-container">
+            <FaGithub
+              color={'black'}
+              classname="link-logo"
+              name="github-logo"
+              size={30}
+            />
+          </div>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/ishaanpuri/"
+        >
+          <div className="link-container">
+            <FaLinkedinIn
+              color={'black'}
+              classname="link-logo"
+              name="linkedin-logo"
+              size={30}
+            />
+          </div>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="mailto:ishaanp9@uw.edu"
+        >
+          <div className="link-container">
+            <MdEmail
+              color={'black'}
+              classname="link-logo"
+              name="email-logo"
+              size={30}
+            />
+          </div>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.instagram.com/ishaanp9/"
+        >
+          <div className="link-container">
+            <AiFillInstagram
+              color={'black'}
+              classname="link-logo"
+              name="instagram-logo"
+              size={30}
+            />
+          </div>
+        </a>
       </div>
 
       {/* Here will be the buttons to my github, linkedin, email, and instagram */}
